d6be7d1807f5642d5c9976e7fb2e6317b83115f2
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-25 13:33:01 +0400

    connected scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app">
-    hello world
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>hello</h1>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -9,13 +15,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
