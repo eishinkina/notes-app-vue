53cf7656d4d6fed344536afe243f8368be1d337c
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-26 18:10:08 +0400

    add component NewNote

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,7 @@
           <h1>{{ title }}</h1>
           <Message v-if="message" :message="message" />
           <!-- new notes -->
-          <div class="new-note">
-            <input v-model="note.title" type="text" />
-            <textarea v-model="note.descr"></textarea>
-            <button @click="addNote">New note</button>
-          </div>
+          <NewNote :note="note" @addNote="addNote"/>
           <!-- note list  -->
           <div class="notes">
             <div class="note" v-for="(note, i) in notes" :key="i">
@@ -31,9 +27,11 @@
 
 <script>
 import Message from './components/Message.vue';
+import NewNote from './components/NewNote.vue';
 export default {
   components: {
-    Message
+    Message,
+    NewNote,
   },
   data() {
     return {
