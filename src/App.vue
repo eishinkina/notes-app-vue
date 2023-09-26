4ea25c7b90fe69c9e979d1f4b1591ac94045aa1a
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-26 17:20:20 +0400

    created a crud app

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,28 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>hello</h1>
+          <h1>{{ title }}</h1>
+          <div class="message" v-if="message">
+            <p>{{ message }}</p>
+          </div>
+          <!-- new notes -->
+          <div class="new-note">
+            <input v-model="note.title" type="text" />
+            <textarea v-model="note.descr"></textarea>
+            <button @click="addNote">New note</button>
+          </div>
+          <!-- note list  -->
+          <div class="notes">
+            <div class="note" v-for="(note, i) in notes" :key="i">
+              <div class="note-header">
+                <h3>{{ note.title }}</h3>
+              </div>
+              <div class="note-body">
+                <p>{{ note.descr }}</p>
+              </div>
+              <span>{{ note.data }}</span>
+            </div>
+          </div>
         </div>
       </section>
     </div>
@@ -12,6 +33,50 @@
 
 <script>
 export default {
+  data() {
+    return {
+      title: "Notes App",
+      message: null,
+      note: {
+        title: "",
+        descr: "",
+      },
+      notes: [
+        {
+          title: "first notes",
+          descr: "description for first notes",
+          data: new Date(Date.now()).toLocaleString(),
+        },
+        {
+          title: "second notes",
+          descr: "description for second notes",
+          data: new Date(Date.now()).toLocaleString(),
+        },
+        {
+          title: "thirds notes",
+          descr: "description for thirds notes",
+          data: new Date(Date.now()).toLocaleString(),
+        },
+      ],
+    }
+  },
+  methods: {
+    addNote() {
+      let { title, descr } = this.note;
+      if (title === "") {
+        this.message = "title can`t be blank!";
+        return false;
+      }
+      this.notes.push({
+        title,
+        descr,
+        data: new Date(Date.now()).toLocaleString(),
+      });
+      this.message = null;
+      this.note.title = "";
+      this.note.descr = "";
+    },
+  },
 }
 </script>
 
